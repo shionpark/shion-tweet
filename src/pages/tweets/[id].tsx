2119237f4b73262312cbd64a38fa58dd2a6afba7
bkkom <seoy1108@gmail.com>
2023-11-07 13:38:11 +0900

    Feat: 생성 기본 페이지 라우트

---
+++ src/pages/tweets/[id].tsx
@@ -0,0 +1,9 @@
+const TweetDetail = () => {
+  return (
+    <>
+      <h1>Tweet Detail</h1>
+    </>
+  );
+};
+
+export default TweetDetail;
---
+++ src/pages/tweets/index.tsx
@@ -0,0 +1,9 @@
+const Tweet = () => {
+  return (
+    <>
+      <h1>Tweet</h1>
+    </>
+  );
+};
+
+export default Tweet;
