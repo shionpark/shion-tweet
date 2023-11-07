9cde3cd9b81d7f4a7eb88b594d5553b1922fce0d
bkkom <seoy1108@gmail.com>
2023-11-07 10:16:09 +0900

    Chore: 설정 전역 SWRConfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import "@/styles/globals.css";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => fetch(url).then((res) => res.json()),
+        refreshInterval: 3000,
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
