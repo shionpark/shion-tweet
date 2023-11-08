7258d4bda6939988e75fbc4ab5e46a8287ae11d4
bkkom <seoy1108@gmail.com>
2023-11-08 12:26:10 +0900

    Feat: 생성 회원가입 Form

--- src/pages/create-account/index.tsx
+++ src/pages/create-account/index.tsx
@@ -1,8 +1,41 @@
+import { useForm } from "react-hook-form";
+
+interface CreateForm {
+  email?: string;
+  name?: string;
+  password?: string;
+}
+
 const CreateAccount = () => {
+  const { handleSubmit, register } = useForm();
+
+  const onValid = (data: CreateForm) => {};
+
   return (
-    <>
-      <h1>Create Account</h1>
-    </>
+    <div>
+      <h1>계정 만들기</h1>
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("email")}
+          placeholder="email"
+          required
+          type="email"
+        ></input>
+        <input
+          {...register("name")}
+          placeholder="name"
+          required
+          type="text"
+        ></input>
+        <input
+          {...register("password")}
+          placeholder="password"
+          required
+          type="password"
+        ></input>
+        <button type="submit">클릭!</button>
+      </form>
+    </div>
   );
 };
 
