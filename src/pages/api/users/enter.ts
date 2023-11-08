53011da6363f67480b38e67cdea8278fa76db377
bkkom <seoy1108@gmail.com>
2023-11-08 17:16:49 +0900

    Feat: 생성 회원가입 API Route, 비밀번호 hashing

--- src/pages/api/users/enter.ts
+++ src/pages/api/users/enter.ts
@@ -1,9 +1,35 @@
 import client from "@/lib/server/client";
 import withHandler from "@/lib/server/withHandler";
+import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body);
+  const { email, name, password } = req.body;
+
+  if (!email || !name) {
+    return res.status(404).end();
+  }
+
+  const user = await client.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (user) {
+    return res.status(409).end();
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  await client.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
   res.status(200).end();
 };
 
